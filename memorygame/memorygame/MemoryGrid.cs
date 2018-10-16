0594a85b877918df2b05554a048e8ae91a7446d8
CFM Cheng <mickycheng16@hotmail.com>
2018-10-16 18:45:18 +0200

    v0.1

--- memorygame/memorygame/MemoryGrid.cs
+++ memorygame/memorygame/MemoryGrid.cs
@@ -7,44 +7,104 @@ using System.Windows.Controls;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Input;
 
 namespace memorygame
 {
     public class MemoryGrid
-    {
-        private Grid GameGrid;
+    {   
+        //ATTRIBUTEN
+        private Grid grid;
+        private const int cols = 4;
+        private const int rows = 4;
 
+        //CONSTRUCTORS
+        /// <summary>
+        /// MemoryGrid bestaat uit een grid met rijen en kolommen, met daarin: Images en Labels
+        /// </summary>
+        /// <param name="grid">de grid</param>
+        /// <param name="cols">kolommen</param>
+        /// <param name="rows">rijen</param>
         public MemoryGrid(Grid grid, int cols, int rows)
         {
-            GameGrid = grid;
+            this.grid = grid;
             InitializeGameGrid(cols, rows);
-            AddLabel();
-        }       
+            AddImages();
+            AddLabel();                  
+        }         
 
+        //METHODEN
+        /// <summary>
+        /// maakt een speelbord aan met aantal kolommen en rijen
+        /// </summary>
+        /// <param name="cols">het aantal kolommen</param>
+        /// <param name="rows">het aantal rijen</param>
         private void InitializeGameGrid(int cols, int rows)
         {
             for (int i = 0; i < rows; i++)
             {
-                GameGrid.RowDefinitions.Add(new RowDefinition());
+               grid.RowDefinitions.Add(new RowDefinition());
             }
             for (int i = 0; i < cols; i++)
             {
-                GameGrid.ColumnDefinitions.Add(new ColumnDefinition());
+               grid.ColumnDefinitions.Add(new ColumnDefinition());
             }
         }
-
+        /// <summary>
+        /// voegt een label toe
+        /// </summary>
         private void AddLabel()
         {
             Label title = new Label();
             title.Content = "Memory";
-            title.FontFamily = new FontFamily("Distant Galaxy");
-            title.FontSize = 40;
+            title.FontFamily = new FontFamily("batman_font/#BatmanForeverAlternate");
+            title.FontSize = 30;
             title.HorizontalAlignment = HorizontalAlignment.Center;
 
-            Grid.SetColumn(title, 1);
-            GameGrid.Children.Add(title);
+            Grid.SetColumn(title, 5);            
+            grid.Children.Add(title);
+        }
+        /// <summary>
+        /// voegt een image toe, klikbaar
+        /// </summary>
+        private void AddImages()
+        {
+            List<ImageSource> images = GetImagesList();
+            for (int row = 0; row < rows; row++)
+            {
+                for(int column = 0; column < cols; column++)
+                {
+                    Image backgroundImage = new Image();
+                    backgroundImage.Source = new BitmapImage(new Uri("Images_Rear/DC_Comics_logo.png", UriKind.Relative));
+                    backgroundImage.Tag = images.First();
+                    images.RemoveAt(0);
+                    backgroundImage.MouseDown += new MouseButtonEventHandler(CardClick);
+                    Grid.SetColumn(backgroundImage, column);
+                    Grid.SetRow(backgroundImage, row);
+                    grid.Children.Add(backgroundImage);
+                }
+            }
+        }
+        
+        private void CardClick(object sender, MouseButtonEventArgs e)
+        {
+            Image card = (Image)sender;
+            ImageSource front = (ImageSource)card.Tag;
+            card.Source = front;
         }
 
+        private List<ImageSource> GetImagesList()
+        {
+            List<ImageSource> images = new List<ImageSource>();
+            for(int i = 0; i < 16; i++)
+            {
+                int imageNR = i % 8 + 1;
+                ImageSource source = new BitmapImage(new Uri("Images_Front/" + imageNR + ".png", UriKind.Relative));
+                images.Add(source);
+            }
+            return images;
+        }
         
+
     }
 }
