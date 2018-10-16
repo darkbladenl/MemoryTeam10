78e7dd4dbb3c93069badfa79e0e6b282ba0b57e1
CFM Cheng <mickycheng16@hotmail.com>
2018-10-16 13:15:10 +0200

    v0.1

---
+++ memorygame/memorygame/MemoryGrid.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace memorygame
+{
+    public class MemoryGrid
+    {
+        private Grid GameGrid;
+
+        public MemoryGrid(Grid grid, int cols, int rows)
+        {
+            GameGrid = grid;
+            InitializeGameGrid(cols, rows);
+            AddLabel();
+        }       
+
+        private void InitializeGameGrid(int cols, int rows)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                GameGrid.RowDefinitions.Add(new RowDefinition());
+            }
+            for (int i = 0; i < cols; i++)
+            {
+                GameGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+        }
+
+        private void AddLabel()
+        {
+            Label title = new Label();
+            title.Content = "Memory";
+            title.FontFamily = new FontFamily("Distant Galaxy");
+            title.FontSize = 40;
+            title.HorizontalAlignment = HorizontalAlignment.Center;
+
+            Grid.SetColumn(title, 1);
+            GameGrid.Children.Add(title);
+        }
+
+        
+    }
+}
