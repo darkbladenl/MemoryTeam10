9d82138801485f82888dc31238db5a3cf4b1eee1
joop339 <joop339degraaf@gmail.com>
2018-10-18 12:53:26 +0200

    klk

--- memorygame/memorygame/MemoryGrid.cs
+++ memorygame/memorygame/MemoryGrid.cs
@@ -20,6 +20,7 @@ namespace memorygame
         //nieuwe instantie van lijst met kaartjes 1 t/m 8 x2
         private List<int> cards = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8 };
         private int score = 0;
+        private int omgedraaid = 0; // aantal omgedraaide kaarten
         //CONSTRUCTORS
         /// <summary>
         /// MemoryGrid bestaat uit een grid met rijen en kolommen, met daarin: Images en Labels
@@ -27,6 +28,7 @@ namespace memorygame
         /// <param name="grid">de grid</param>
         /// <param name="cols">kolommen</param>
         /// <param name="rows">rijen</param>
+
         public MemoryGrid(Grid grid, int cols, int rows)
         {
             this.grid = grid;
@@ -35,7 +37,6 @@ namespace memorygame
             AddLabel();
             
         }         
-
         //METHODEN
         /// <summary>
         /// maakt een speelbord aan met aantal kolommen en rijen
@@ -56,16 +57,19 @@ namespace memorygame
         /// <summary>
         /// voegt een label toe
         /// </summary>
-        private void AddLabel()
+        public void AddLabel()
         {
-            Label title = new Label();
-            title.Content = score;
-            title.FontFamily = new FontFamily("batman_font/#BatmanForeverAlternate");
-            title.FontSize = 30;
-            title.HorizontalAlignment = HorizontalAlignment.Center;
+            while (true)
+            {
+                Label title = new Label();
+                title.Content = score;
+                title.FontFamily = new FontFamily("batman_font/#BatmanForeverAlternate");
+                title.FontSize = 30;
+                title.HorizontalAlignment = HorizontalAlignment.Center;
 
-            Grid.SetColumn(title, 5);            
-            grid.Children.Add(title);
+                Grid.SetColumn(title, 5);
+                grid.Children.Add(title);
+            }
         }
         /// <summary>
         /// voegt images toe, klikbaar
@@ -94,8 +98,11 @@ namespace memorygame
             Image card = (Image)sender;
             ImageSource front = (ImageSource)card.Tag;
             card.Source = front;
-            
+            score++;
+            omgedraaid++;
+            MessageBox.Show (score.ToString());
         }
+
         /// <summary>
         /// plaatst images in een willekeurige volgorde
         /// </summary>
