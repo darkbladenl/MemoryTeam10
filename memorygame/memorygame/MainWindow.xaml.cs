750267a2521ba8d712ee7120d9e54ad3a3de3b15
joop339 <joop339degraaf@gmail.com>
2018-10-22 17:19:52 +0200

    ddd

--- memorygame/memorygame/MainWindow.xaml.cs
+++ memorygame/memorygame/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,14 +25,19 @@ namespace memorygame
         private const int rows = 4;
         MemoryGrid grid;
         
+        
+        
 
         public MainWindow()
         {
             InitializeComponent();
             grid = new MemoryGrid(GameGrid, cols, rows);
-            
+            //var notificationSound = new SoundPlayer(Properties.Resources.Nice_Meme);
+            //notificationSound.PlaySync();
 
 
         }
+        
+
     }   
 }
