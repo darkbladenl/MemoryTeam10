27af86a61e3e408077c3b8d1ecd83e75119b74e1
Reinderp <rfvisser1@hotmail.com>
2018-10-22 16:08:56 +0200

    Beginscherm
    Compleet beginscherm v1

--- memorygame/memorygame/Window1.xaml.cs
+++ memorygame/memorygame/Window1.xaml.cs
@@ -23,12 +23,45 @@ namespace memorygame
         {
             InitializeComponent();
         }
+        public string Gamemode;
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+
             MainWindow MainWindow = new MainWindow();
             MainWindow.Show();
             this.Close();
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Naambox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string Inhoud = Naambox.Text;
+
+            if (Inhoud != "") 
+            {
+                BtnSpelen.IsEnabled = true;
+            }
+        }
+
+        private void Scores_Click(object sender, RoutedEventArgs e)
+        {
+            Gamemode = "Scores";
+            Scores.Background = Scores.Background == Brushes.Red ? (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFDDDDDD")) : Brushes.Red;
+            Timer.Background = Timer.Background == Brushes.LightGray ? (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFDDDDDD")) : Brushes.LightGray;
+
+        }
+
+        private void Timer_Click(object sender, RoutedEventArgs e)
+        {
+            Gamemode = "Timer";
+            Timer.Background = Timer.Background == Brushes.Red ? (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFDDDDDD")) : Brushes.Red;
+            Scores.Background = Scores.Background == Brushes.LightGray ? (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFDDDDDD")) : Brushes.LightGray;
+
         }
     }
 }
