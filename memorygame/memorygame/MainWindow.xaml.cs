4798f5e78d10b0cf1efff94254984ca0b5988ebf
Drochot <drochot@hotmail.com>
2018-10-22 16:01:08 +0200

    save prototype

--- memorygame/memorygame/MainWindow.xaml.cs
+++ memorygame/memorygame/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Media;
@@ -13,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Windows.Markup;
 
 namespace memorygame
 {
@@ -24,9 +27,9 @@ namespace memorygame
         private const int cols = 5;
         private const int rows = 4;
         MemoryGrid grid;
-        
-        
-        
+
+
+
 
         public MainWindow()
         {
@@ -34,10 +37,45 @@ namespace memorygame
             grid = new MemoryGrid(GameGrid, cols, rows);
             //var notificationSound = new SoundPlayer(Properties.Resources.Nice_Meme);
             //notificationSound.PlaySync();
-
+            Loaded += new RoutedEventHandler(MainWindow_Loaded);
+            Closing += new CancelEventHandler(MainWindow_Closing);
 
         }
-        
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadExternalXaml();
+        }
 
-    }   
+        void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            SaveExternalXaml();
+        }
+
+
+        public void LoadExternalXaml()
+        {
+            if (File.Exists(@"C:\Test.xaml"))
+            {
+                using (FileStream stream = new FileStream(@"C:\Test.xaml", FileMode.Open))
+                {
+                    this.Content = XamlReader.Load(stream);
+                }
+            }
+        }
+
+        public void SaveExternalXaml()
+        {
+            using (FileStream stream = new FileStream(@"C:\Test.xaml", FileMode.Create))
+            {
+                XamlWriter.Save(this.Content, stream);
+            }
+        }
+
+
+
+
+
+
+
+    }
 }
