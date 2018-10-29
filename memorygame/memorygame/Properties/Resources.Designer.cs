94613704a9cc5f40d769a5377f730737d7153bdf
CFM Cheng <mickycheng16@hotmail.com>
2018-10-29 11:19:44 +0100

    .

--- memorygame/memorygame/Properties/Resources.Designer.cs
+++ memorygame/memorygame/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace memorygame.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace memorygame.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("memorygame.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace memorygame.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,10 +63,9 @@ namespace memorygame.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap achtergrond_1 {
+        public static System.Drawing.Bitmap achtergrond_1 {
             get {
                 object obj = ResourceManager.GetObject("achtergrond_1", resourceCulture);
-
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
